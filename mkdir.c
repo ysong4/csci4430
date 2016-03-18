d7e8a4c20ef2d21570e1e857377cd581b70e925d
Jacky <jacky@Ubuntu14.04.3>
2016-03-18 17:58:15 +0800

    addedcrypt

--- mkdir.c
+++ mkdir.c
@@ -2,11 +2,37 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <string.h>
+#include <crypt.h>
+#include <stdlib.h>
 
-main(){
+main(int argc, char * argv[]){
+
+	char filename[23];
+	int i;
+	strncpy(filename, crypt(argv[1], "$1$00$")+6, 23);
+	for (i=0; i<22; ++i){
+		if (filename[i] == '/'){
+			filename[i] = '_';
+		}
+	}
+
 	struct stat st = {0};
-	
+
 	if (stat("./proxyFiles", &st) == -1){
 		mkdir("./proxyFiles", 0777);
 	}
+
+
+	char partOne[] = "./proxyFiles/";
+	char * dirName;
+	dirName = malloc((strlen(partOne)+ strlen(filename))* sizeof(char));
+	dirName = strcat(partOne, filename);
+
+
+	if (stat(dirName, &st) == -1){
+		mkdir(dirName, 0777);
+	}
+
+	printf("%s\n", dirName);
 }
